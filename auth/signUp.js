2f08a5ba44e123e385e50006cb95c7aa595bef4e
KelahKelah <kelechiekebor@gmail.com>
2020-09-22 08:32:07 +0100

    create sign up page

---
+++ auth/signUp.js
@@ -0,0 +1,19 @@
+class checkUser {
+    constructor() {
+
+    }
+}
+
+class User {
+    constructor(id, fullName, email, number, password) {
+        this.id = id
+        this.fullName = fullName
+        this.email = email
+        this.number = number
+        this.password = password
+    }
+}
+
+// REGISTER 
+const register = () => {
+}
