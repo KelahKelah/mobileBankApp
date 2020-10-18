5a03d9462e882171f516f9582ee6d5aa49296214
KelahKelah <kelechiekebor@gmail.com>
2020-10-18 13:46:16 +0100

    dynamically fetching table data for daily transactions

--- utiils/data.js
+++ utiils/data.js
@@ -1,10 +1,11 @@
 const dailyTransactions = [
-    {id:1,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 200, transfer: 'debit'},    
+    {id:1,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 200, transfer: 'debit', date: 'sept 2020,1:30'},    
     {id:2,transactionType : 'commision', channel: 'ussd', branch: ' e-channel', remark: 'recovery charge', amount: 200, transfer: 'debit'},
-    {id:3,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 2000, transfer: 'credit'},
+    {id:3,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 2000, transfer: 'credit', date: 'sept 2020,1:30'},
     {id:4,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'gym', amount: 5000, transfer: 'debit'},
     {id:5,transactionType : 'commision', channel: 'mobile banking', branch: ' atm', remark: 'grocceries', amount: 4000, transfer: 'credit'},
-    {id:6,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 1200, transfer: 'debit'},
+    {id:6,transactionType : 'transaction between customers', channel: 'mobile banking', branch: ' e-channel', remark: 'schools fees', amount: 1200, transfer: 'debit', date: 'sept 2020,1:30'},
     {id:7,transactionType : 'commision', channel: 'ussd', branch: ' e-channel', remark: 'recovery charge', amount: 200, transfer: 'debit'},
 ]
-export {dailyTransactions}
+module.exports = dailyTransactions
+// module.exports = Color;
